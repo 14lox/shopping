f1d95fd32d344d422ebb175d257a5ddad75b8ea3
harry gao <foxgaocn@gmail.com>
2013-10-13 21:10:09 +1100

    local storage

--- server/public/script/habitor.js
+++ server/public/script/habitor.js
@@ -1,24 +1,28 @@
 $(function(){
-    var itemNum = 1;
-   //create habit form behavior
+    var itemNum;
+    
+    //create habit form behavior
    $("#submitButton").click(function(event){
       event.preventDefault();
       var item = $("#item").val().replace(/^\s+|\s+$/g, '');
       if(item == '') return;
       $("#item").val('');//clear input box
+      itemNum++;
+      localStorage.setItem("itemNum", itemNum);
 
       var itemId = "item" + itemNum;
       var dataId = "data" + itemNum;
       var strip = itemNum % 2 == 0 ? "gray" : "white"
 
-      $("#mylist").append("<p class='item " + strip +"' id='" + itemId+"'><b>" + item + "</b></p>");
-      
+      var para = $('<p>' + item + '</p>').attr( {"class": strip + ' item', id: itemId});
+      $("#mylist").append(para);
+      localStorage.setItem(itemId, item);
 
       //do the query to get promotion
       var posting = $.post("/promotion/post_query", {query: item});
       posting.done(function( data ) {
           if(data.promotions.length == 0) return;
-          
+          localStorage.setItem(dataId, JSON.stringify(data));
           $("#data").append("<div id='" + dataId + "'>");
           data.promotions.forEach(function(promotion){
               $("#"+dataId).append( "<p><i>" + promotion.supplier + ":&nbsp;&nbsp;</i>" + promotion.content + "</p>" );
@@ -26,7 +30,7 @@ $(function(){
           //append show promotion to the item
           $("#"+itemId).append("<a class='showPromotion' href='#'><img src='/image/sale.png'></a>"); 
       });
-      itemNum++;
+      
    });
 
   $(document).on('click', '.showPromotion', function(){
@@ -38,5 +42,38 @@ $(function(){
       modal: true
     });
   })
+  
+
+  loadData = function()
+  {
+    itemNum = localStorage.getItem("itemNum");
+    if(itemNum == undefined)
+    {
+      itemNum = 0;
+      return;
+    }
+
+    for(i = 1; i<=itemNum; i++)
+    {
+      var itemId = "item" + i;
+      var item = localStorage.getItem(itemId);
+      var strip = i % 2 == 0 ? "gray" : "white"
+
+      var para = $('<p>' + item + '</p>').attr( {"class": strip + ' item', id: itemId});
+      $("#mylist").append(para);
+
+      var dataId = "data" + i;
+      var data = JSON.parse(localStorage.getItem(dataId));
+      if(data == null || data.promotions.length == 0 ) continue;
+      $("#data").append("<div id='" + dataId + "'>");
+      data.promotions.forEach(function(promotion){
+          $("#"+dataId).append( "<p><i>" + promotion.supplier + ":&nbsp;&nbsp;</i>" + promotion.content + "</p>" );
+      });
+      //append show promotion to the item
+      $("#"+itemId).append("<a class='showPromotion' href='#'><img src='/image/sale.png'></a>");
+    }
+  }
+
+  loadData();
 
 });
