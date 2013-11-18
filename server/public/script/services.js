850aa0d6a2514b1f27843c445862454f8df4d3e4
Haiyang (harryg) Gao <harrygao.oz@gmail.com>
2013-11-18 20:33:24 +1100

    fix bug: default value for active supplier

--- server/public/script/services.js
+++ server/public/script/services.js
@@ -7,7 +7,7 @@ services.provider('activeSupplierService', function () {
 		
 	this.suppliers = {'Coles':1, 'Wollies':2};
 	this.activeSuppliers = parseInt(localStorage['activeSuppliers']);
-  	if(this.activeSuppliers == NaN){
+  	if(isNaN(this.activeSuppliers)){
     	//default to select all, so it is 2^n - 1
     	this.activeSuppliers = Math.pow(2, _.keys(suppliers).length) - 1;
   	}
