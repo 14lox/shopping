d7f1f4507cf1dd21f8df76838be47f0f6039caf3
Haiyang (harry) Gao <foxgaocn@gmail.com>
2014-03-05 21:42:31 +1100

    add exception handling in main program

--- scraper/scrapper.rb
+++ scraper/scrapper.rb
@@ -2,15 +2,25 @@ require_relative 'lib/coles_scraper'
 require_relative 'lib/ww_scraper'
 require_relative 'lib/sql_writter'
 
+begin
+  puts "start crawling from coles..."
+  cole_scraper = ColesScraper.new
+  cole_scraper.get_post_query
+  cole_scraper.get_data
+  cole_scraper.write
+rescue
+  puts "an error occured in coles scrapper"
+end
 
-cole_scraper = ColesScraper.new
-cole_scraper.get_post_query
-cole_scraper.get_data
-cole_scraper.write
+begin
+  puts "start crawling from Woolworth..."
+  s = WwScraper.new
+  s.get_data
+  s.write
+rescue
+  puts "an error occured in coles scrapper"
+end
 
-s = WwScraper.new
-s.get_data
-s.write
 
 SqlWritter.write
 
